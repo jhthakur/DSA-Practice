c8ef04e9757d8e55afd7b20e975d587a1ea40cbe
Jai Harsh <jaiharshthakur17@gmail.com>
2026-06-24 07:42:13 +0530

    Add files via upload

---
+++ MergeSorting.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void merge(vector<int>&a,vector<int>&b,vector<int>&v){
+    int i=0,j=0,k=0;
+    while(i<a.size()&&j<b.size()){
+        if(a[i]<b[j]){
+            v[k]=a[i];
+            i++; k++; }
+        else{
+            v[k]=b[j];
+            j++; k++; }}
+    while(i<a.size()){
+        v[k]=a[i];
+        i++; k++;}
+    while(j<b.size()){
+        v[k]=b[j];
+        j++; k++; }
+}
+void mergeSort(vector<int>&v){
+    int n=v.size();
+    if(n==1)return;
+    int n1=n/2;int n2=n-n1;
+    vector<int>a(n1),b(n2);
+    for(int i=0;i<n1;i++){
+        a[i]=v[i];}
+    for(int i=0;i<n2;i++){
+        b[i]=v[n1+i];}
+    mergeSort(a);
+    mergeSort(b);
+    merge(a,b,v);
+}
+int main(){
+    int arr[]={2,5,3,8,5,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int>v(arr,arr+n);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }cout<<endl;
+    mergeSort(v);
+     for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+}
