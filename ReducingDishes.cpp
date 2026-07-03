e13a9e39d6740553f8d662ae4a8a9246e4ceea50
Jai Harsh <jaiharshthakur17@gmail.com>
2026-07-03 07:12:19 +0530

    Add files via upload

---
+++ ReducingDishes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int n=satisfaction.size();
+        sort(satisfaction.begin(),satisfaction.end());
+        int suff[n];
+        int idx=-1;
+        suff[n-1]=satisfaction[n-1];
+        for(int i=n-2;i>=0;i--){
+            suff[i]=suff[i+1]+satisfaction[i];
+        }
+        for(int i=0;i<n;i++){
+             if(suff[i]>=0){idx=i;
+            break;}
+        }
+        if(idx==-1)return 0;
+        int x=1;
+        int ans=0;
+        for(int i=idx;i<n;i++){
+            ans+=satisfaction[i]*x;
+            x++;
+        }
+        return ans;
+    }
+};
