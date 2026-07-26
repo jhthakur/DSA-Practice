a1a88a461f6c94a92b4318d6da549c2939efe83f
Jai Harsh <jaiharshthakur17@gmail.com>
2026-07-26 09:39:17 +0530

    Add files via upload

---
+++ DistinctPrimeNo.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void filler(vector<bool>& sieve){
+        int n=sieve.size();
+        for(int i=2;i<=sqrt(n);i++){
+            for(int j=i*2;j<n;j+=i){
+                sieve[j]=0;
+            }
+        }
+    }
+    int distinctPrimeFactors(vector<int>& nums) {
+      int n=nums.size();
+      int mx=0;
+      for(int i=0;i<n;i++){
+        mx=max(nums[i],mx);
+      }  
+      vector<bool> sieve(mx+1,1);
+      filler(sieve);
+      vector<int> prime;
+      for(int i=2;i<sieve.size();i++){
+        if(sieve[i]==1)prime.push_back(i);
+      }
+      vector<bool> taken(prime.size(),0);
+      for(int i=0;i<n;i++){
+        for(int j=0;j<prime.size();j++){
+            if(prime[j]>nums[i])break;
+            if(nums[i]%prime[j]==0)taken[j]=1;
+        }
+      }
+      int count=0;
+      for(int i=0;i<taken.size();i++){
+        if(taken[i]==1)count++;
+      }
+      return count;
+    }
+};
