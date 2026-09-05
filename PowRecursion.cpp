5e583467d78d0c470e10c9eaaa2c33304b30dc11
Jai Harsh <jaiharshthakur17@gmail.com>
2026-09-05 10:24:02 +0530

    Add files via upload

---
+++ PowRecursion.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) { 
+        long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        if(N==0)return 1;
+        double Half=myPow(x,N/2);
+        if(N%2==0){
+            return Half*Half;
+        }
+        else{
+            return x*Half*Half;
+        }
+    }
+};
