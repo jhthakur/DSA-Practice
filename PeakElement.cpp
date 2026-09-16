5c896310e586ef02b5c880bb8ae0fda133040d61
Jai Harsh <jaiharshthakur17@gmail.com>
2026-09-16 13:26:55 +0530

    Add files via upload

---
+++ PeakElement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int lo=0;
+        int hi=n-1;
+        while(lo<hi){
+           int mid=lo+(hi-lo)/2;
+            if(nums[mid]<nums[mid+1])lo=mid+1;
+            else hi=mid;}
+    return lo;}
+};
